22133f63c30631b09a2ca94e7b89ecbcc4356723
Jimmy Ou <yanagiis@gmail.com>
2022-08-16 01:10:57 +0800

    feat: remove darkplus.nvim

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -55,7 +55,6 @@ return packer.startup(function(use)
 
   -- Colorschemes
   use { "folke/tokyonight.nvim" }
-  use { "lunarvim/darkplus.nvim" }
 
   -- cmp plugins
   use { "hrsh7th/nvim-cmp" } -- The completion plugin
