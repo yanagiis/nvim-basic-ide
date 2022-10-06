9e920b0769d460353c7b40a96cd35cca1590f73c
jimmy.ou <yanagiis@gmail.com>
2022-10-06 16:43:03 +0800

    feat: update for new lsp

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -73,19 +73,19 @@ end
 
 M.on_attach = function(client, bufnr)
   if client.name == "tsserver" then
-    client.resolved_capabilities.document_formatting = false
+    client.server_capabilities.documentFormattingProvider = false
   end
 
   if client.name == "sumneko_lua" then
-    client.resolved_capabilities.document_formatting = false
+    client.server_capabilities.documentFormattingProvider = false
   end
 
   if client.name == "gopls" then
-    client.resolved_capabilities.document_formatting = false
+    client.server_capabilities.documentFormattingProvider = false
   end
 
   if client.name == "rust_analyzer" then
-    client.resolved_capabilities.document_formatting = false
+    client.server_capabilities.documentFormattingProvider = false
   end
 
   lsp_keymaps(bufnr)
